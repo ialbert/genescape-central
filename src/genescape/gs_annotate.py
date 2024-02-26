53d5a91ebbb2371b6962b532f5e283c4ac85ed64
Istvan Albert <istvan.albert@gmail.com>
2024-02-26 10:25:09 -0500

    update

--- src/genescape/gs_annotate.py
+++ src/genescape/gs_annotate.py
@@ -4,7 +4,7 @@ Annotates a list of genes with functions based on the GO graph
 import csv
 import gzip
 import sys,json,re
-from collections import Counter
+from collections import Counter, OrderedDict
 from itertools import *
 from pprint import pprint
 
@@ -12,7 +12,7 @@ from genescape import utils
 from genescape.utils import GOID, LABEL
 
 
-def run(fname=utils.GENE_LIST, index=utils.INDEX, top=10, verbose=False, match='', minc=1):
+def run(fname=utils.GENE_LIST, index=utils.INDEX, top=10, verbose=False, match='', minc=1, output=sys.stdout):
 
     # Open the index stream
     idx_stream = gzip.open(index, mode="rt", encoding="UTF-8")
@@ -21,80 +21,94 @@ def run(fname=utils.GENE_LIST, index=utils.INDEX, top=10, verbose=False, match='
     data = json.load(idx_stream)
 
     # Read the genelist
-    stream = utils.get_stream(fname)
-    stream = map(lambda x: x.upper(), stream)
-    stream = list(stream)
+    names = utils.get_stream(fname)
+    names = map(lambda x: x.upper(), names)
+    names = list(names)
 
+    # The valid ids are the unqiue gene and protein ids.
     valid_ids = set(list(data[utils.IDX_gene2go].keys()) + list(data[utils.IDX_prot2go].keys()))
 
-    # Fetch functions stored for genes or proteins
-    def get(elem):
-        return set(data[utils.IDX_gene2go].get(elem, []) + data[utils.IDX_prot2go].get(elem, []))
+    # Fetch GO functions for a given gene or protein id.
+    def get_func(name):
+        ids = set(data[utils.IDX_gene2go].get(name, []) + data[utils.IDX_prot2go].get(name, []))
+        return ids
 
-    miss, coll = [], []
+    # The missing and collected gene names.
+    miss, coll, func2name = [], [], {}
 
-    for elem in stream:
-
-        if elem not in valid_ids:
-            miss.append(elem)
+    # Collect the missing elements.
+    for name in names:
+        if name not in valid_ids:
+            miss.append(name)
         else:
-            coll.extend(get(elem))
+            funcs = get_func(name)
+            # Collect genes that match a function.
+            for func in funcs:
+                func2name.setdefault(func, []).append(name)
+            coll.extend(funcs)
 
+    # The missing ids.
     if miss:
         utils.warn(f"missing {len(miss)} ids: {','.join(miss[:10])} ... ")
+
+    # Print all missing elements if verbose.
     if verbose:
         utils.warn(f"all missing ids: {miss} ")
 
+    # Get the elements for a go id.
     def go2func(goid):
         return data[utils.IDX_OBO][goid]["name"]
 
     # Build the counter.
-    count = Counter(coll)
+    counter = Counter(coll)
 
     # Keep the sort stable.
-    elems = count.most_common()
+    counts = counter.most_common()
 
     # Fill in the function names.
-    elems = map(lambda x: (x[0], x[1], go2func(x[0])), elems)
+    counts = map(lambda x: (x[0], x[1], go2func(x[0])), counts)
 
     # Apply the regex filter
-    elems = filter(lambda x: re.search(match, x[2]), elems) if match else elems
+    counts = filter(lambda x: re.search(match, x[2]), counts) if match else counts
 
     # Apply the minimum count filter
-    elems = filter(lambda x: x[1] >= minc, elems)
+    counts = filter(lambda x: x[1] >= minc, counts)
 
     # Sort the remaining elements by count.
-    elems = sorted(elems, key=lambda x: (x[1], x[0]), reverse=True)
+    counts = sorted(counts, key=lambda x: (x[1], x[0]), reverse=True)
 
     # The number of hits found.
-    n_found = len(list(elems))
+    n_found = len(list(counts))
 
     # Take the top N elements.
-    elems = elems[:top] if top > 0 else elems
+    counts = counts[:top] if top > 0 else counts
 
     # The size of the original collection.
-    size = len(stream)
+    n_size = len(names)
 
     # Create the data object
-    result = []
-    fields_long = [utils.GOID, "count", "size", utils.LABEL, "function"]
-    for goid, cnt, func in elems:
-        if match and re.search(match, func) is None:
-            continue
+    res = []
+    data_fields = [utils.GOID, "count", "size", utils.LABEL, "function", "genes"]
+    for goid, cnt, func in counts:
         label = f"{cnt}"
-        elem = dict(zip(fields_long, [goid, cnt, size, label, func]))
-
-        result.append(elem)
+        funcs = func2name.get(goid, [])
+        name = dict(zip(data_fields, [goid, cnt, n_size, label, func, funcs]))
+        res.append(name)
 
     # The CSV file has fewer fields
-    fields_short = fields_long
-    write = csv.DictWriter(sys.stdout, fieldnames=fields_short)
-    write.writeheader()
-    write.writerows(result)
+    if output is not None:
+        csv_field = data_fields
+        write = csv.DictWriter(output, fieldnames=csv_field)
+        write.writeheader()
+        write.writerows(res)
 
     # Final notification if needed.
-    if len(elems) != n_found:
-        utils.info(f"{len(elems)} out of {n_found} shown")
+    if len(counts) != n_found:
+        utils.info(f"{len(counts)} out of {n_found} shown")
+
+    print (res)
+
+    return res, miss
 
 
 if __name__ == "__main__":
